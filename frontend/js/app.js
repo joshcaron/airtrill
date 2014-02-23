07041240375c7e0a58710c7df414327cfc52cefa
Christopher Jelly <chris.jelly@gmail.com>
2014-02-23 09:42:18 -0500

    using current track ids and skip list rather than urls

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,7 +7,7 @@ jukebox.service("RequestedSongs", function($http) {
       $http.jsonp("http://music.turtles.io/texts?callback=JSON_CALLBACK").
         success(function(data, status, headers, config) {
           count = data.length;
-          $.each(data, function(i, query) {
+          $.each(_.sortBy(data, 'date'), function(i, query) {
              SC.get('/tracks', { q: query.song }, function(tracks) {
                songs.push(tracks[0]);
                if (!--count) callback(songs);
@@ -21,26 +21,51 @@ jukebox.service("RequestedSongs", function($http) {
   }
 });
 
+jukebox.service("AccountService", function($http) {
+  return {
+    registerAccount: function(country, name, callback) {
+      $http.jsonp("http://music.turtles.io/new_account?country=" + country + "&name=" + name + "&callback=JSON_CALLBACK").
+        success(function(data, status, headers, config) {
+          callback(data);
+        }).
+        error(function(data, status, headers, config) {
+          callback(false);
+        });
+    }
+  }
+});
+
+jukebox.controller('McJukeboxRegisterControl', function($scope, AccountService) {
+  $scope.country = "US";
+  $scope.room_name = "";
+  
+  $scope.haveCookie = function() {
+    return $.cookie("account") | false;
+  };
+  
+  $scope.logout = function() {
+    $.setCookie("account", {});
+  };
+  
+  $scope.register = function() {
+    AccountService.registerAccount($scope.country, $scope.room_name, function(response) {
+      if (response) {
+        $scope.room_name = response.account.name;
+        $scope.phone = response.account.phone.friendly_name;
+        $.setCookie("account", response);
+      }
+    });
+  }
+});
+
 jukebox.controller('McJukeboxControl', function ($scope) {
   $scope.play = 0;
 });
 
-jukebox.controller('McJukeboxWrapper', function ($scope, RequestedSongs) {
-  $scope.number = "+1 617 555 1234";
+jukebox.controller('McJukeboxWrapper', function ($scope, $timeout, RequestedSongs) {
+  $scope.number = "1 (508) 762-4751";
   $scope.base_url = "https://w.soundcloud.com/player/?auto_play=true&url="
-  
-  RequestedSongs.getRequestedSongs(function(songs) {
-    $scope.playlist = songs;
-    $scope.$apply();
-  });
-  
-  $scope.loadTrack = function($event) {
-    $scope.$broadcast('playTrack', {url: $($event.target).data('url')});
-  }
-});
-
-jukebox.controller('McJukeboxPlaylistControl', function ($scope, $http) {
-
+  $scope.playlist_pointer = 0;
 });
 
 jukebox.controller('McJukeboxCurrentControl', function ($scope) {
@@ -53,6 +78,54 @@ jukebox.controller('McJukeboxCurrentControl', function ($scope) {
     }
   });
 
+});
+
+jukebox.controller('McJukeboxPlaylistControl', function($scope, $timeout, RequestedSongs) {
+  
+  $scope.playlist = [];
+  $scope.playlist_ids = [];
+  
+  $scope.current_track_id = 0;
+  $scope.playlist_queue = []; // contains the IDs of songs we want to play
+  
+  $scope.skipped_tracks = []; // contains the IDs of songs we have skipped
+  
+  $scope.first_load = true;
+  
+  $scope.periodicRequest = function() {
+    $timeout(function() {
+      RequestedSongs.getRequestedSongs(function(songs) {
+        $scope.playlist = songs;
+        $scope.playlist_queue = $scope.playlist_ids = _.pluck($scope.playlist, 'id');
+        $scope.$apply();
+        
+        if ($scope.first_load) {
+          $scope.current_track_id = _.first($scope.playlist).id;
+          popSongStack();
+          $scope.first_load = false;
+        }
+        
+        $scope.$apply();
+      });
+    }, 1000)
+  };
+  
+  popSongStack = function() {
+    $scope.skipped_tracks.push($scope.current_track_id);
+    $scope.playlist_queue = _.difference($scope.playlist_queue, $scope.skipped_tracks);
+    $scope.current_track_id = _.first($scope.playlist_queue);
+    console.log($scope.playlistToDisplay());
+    $scope.$apply();
+  };
+  
+  $scope.periodicRequest();
+  
+  $scope.playlistToDisplay = function() {
+    _.filter($scope.playlist, function(track) {
+      _.contains($scope.playlist_queue, track.id);
+    });
+  }
+
   $scope.pause = function() {
     $scope.$broadcast("pause");
   };
@@ -60,6 +133,14 @@ jukebox.controller('McJukeboxCurrentControl', function ($scope) {
   $scope.play = function() {
     $scope.$broadcast("play");
   };
+
+  $scope.skip = function() {
+    popSongStack();
+  };
+  
+  $scope.$on('songFinished', function(e) {
+    popSongStack();
+  });
   
 });
 
@@ -73,15 +154,23 @@ jukebox.controller('McJukeboxSoundCloudPlayerControl', function($scope, $sce) {
     return $scope.widget;
   };
   
-  $scope.$on('playTrack', function(e, track) {
-    $scope.track_url = $sce.trustAsResourceUrl($scope.base_url + track.url);
-    widget().load($scope.track_url, {callback: function() {
-      widget().getCurrentSound(function(currentSound) {
-        $scope.$emit('trackPlaying', currentSound);
-      });
-    }});
+  $scope.$watch('current_track_id', function() {
+    newId = $scope.current_track_id;
+    if (newId > 0) {
+      playSong = _.where($scope.playlist, {id: newId})[0]
+      $scope.track_url = $sce.trustAsResourceUrl($scope.base_url + playSong.permalink_url)
+      widget().load($scope.track_url, {callback: function() {
+        widget().bind(SC.Widget.Events.FINISH, function() {
+          $scope.$emit('songFinished');
+        })
+      
+        widget().getCurrentSound(function(currentSound) {
+          $scope.$emit('trackPlaying', currentSound);
+        });
+      }});
+    }
   });
-  
+
   $scope.$on('pause', function() {
     widget().pause();
   });
