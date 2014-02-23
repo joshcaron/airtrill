ada092418ad06a47e73fead2397a066c610a418f
Christopher Jelly <chris.jelly@gmail.com>
2014-02-23 06:38:16 -0500

    music works

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,34 +1,96 @@
 var jukebox = angular.module('McJukeboxApp', []);
 
+jukebox.service("RequestedSongs", function($http) {
+  return {
+    getRequestedSongs: function(callback) {
+      songs = []
+      $http.jsonp("http://music.turtles.io/texts?callback=JSON_CALLBACK").
+        success(function(data, status, headers, config) {
+          count = data.length;
+          $.each(data, function(i, query) {
+             SC.get('/tracks', { q: query.song }, function(tracks) {
+               songs.push(tracks[0]);
+               if (!--count) callback(songs);
+             });
+          });
+        }).
+        error(function(data, status, headers, config) {
+          callback([]);
+        });
+    }
+  }
+});
+
 jukebox.controller('McJukeboxControl', function ($scope) {
   $scope.play = 0;
 });
 
-jukebox.controller('McJukeboxWrapper', function ($scope) {
+jukebox.controller('McJukeboxWrapper', function ($scope, RequestedSongs) {
   $scope.number = "+1 617 555 1234";
-  $scope.base_url = "https://w.soundcloud.com/player/?url="
-});
-
-jukebox.controller('McJukeboxPlaylistControl', function ($scope) {
-  $scope.tracks = [];
-});
-
-jukebox.controller('McJukeboxCurrentControl', function ($scope, $sce) {
-  $scope.track = "http://api.soundcloud.com/pitbull/pitbull-timber-featuring-ke-ha"
+  $scope.base_url = "https://w.soundcloud.com/player/?auto_play=true&url="
   
-  $scope.elem = "sc-widget";
-
-  getIframe = function() {
-    return $('#' + $scope.elem)[0]
+  RequestedSongs.getRequestedSongs(function(songs) {
+    $scope.playlist = songs;
+    $scope.$apply();
+  });
+  
+  $scope.loadTrack = function($event) {
+    $scope.$broadcast('playTrack', {url: $($event.target).data('url')});
   }
+});
+
+jukebox.controller('McJukeboxPlaylistControl', function ($scope, $http) {
+
+});
+
+jukebox.controller('McJukeboxCurrentControl', function ($scope) {
+    
+  $scope.$on('trackPlaying', function(e, songInfo) {
+    if (songInfo) {
+      $scope.title = songInfo.title;
+      $scope.artwork = songInfo.artwork_url;
+      $scope.$apply();
+    }
+  });
 
   $scope.pause = function() {
-    SC.Widget(getIframe()).pause();
+    $scope.$broadcast("pause");
   };
   
   $scope.play = function() {
-    SC.Widget(getIframe()).play();
-  }
+    $scope.$broadcast("play");
+  };
   
-  $scope.track_url = $sce.trustAsResourceUrl($scope.base_url + $scope.track);
+});
+
+jukebox.controller('McJukeboxSoundCloudPlayerControl', function($scope, $sce) {
+  
+  // Memoized widget returner
+  widget = function() {
+    if (!$scope.widget) {
+      $scope.widget = SC.Widget($('#sc-widget')[0]);
+    }
+    return $scope.widget;
+  };
+  
+  $scope.$on('playTrack', function(e, track) {
+    $scope.track_url = $sce.trustAsResourceUrl($scope.base_url + track.url);
+    widget().load($scope.track_url, {callback: function() {
+      widget().getCurrentSound(function(currentSound) {
+        $scope.$emit('trackPlaying', currentSound);
+      });
+    }});
+  });
+  
+  $scope.$on('pause', function() {
+    widget().pause();
+  });
+  
+  $scope.$on('play', function() {
+    widget().play();
+  });
+  
+  $scope.init = function() {
+    console.log("Init sound player");
+  };
 });
